158e5bb0f3e9677dcaeceda95520fc4629050539
Ruslan Melnyk <kanevgoga2@gmail.com>
2026-01-29 13:39:35 +0200

    fix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://hrloom.agency/sitemap.xml',
+        sitemap: 'https://hrloom.net/sitemap.xml',
     };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://hrloom.agency',
+            url: 'https://hrloom.net',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1,
