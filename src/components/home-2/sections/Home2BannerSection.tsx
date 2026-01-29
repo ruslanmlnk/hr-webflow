f8378964e279170a9082bdcd4a31cbc355e98390
Ruslan Melnyk <kanevgoga2@gmail.com>
2026-01-29 21:27:53 +0200

    rkok

--- src/components/home-2/sections/Home2BannerSection.tsx
+++ src/components/home-2/sections/Home2BannerSection.tsx
@@ -30,7 +30,7 @@ export default function Home2BannerSection() {
                 </p>
               </div>
             </form> */}
-            <a href="#contact-form" onClick={scrollToContactForm} className="primary-button-black w-inline-block">
+            <a href="!#" onClick={scrollToContactForm} className="primary-button-black w-inline-block">
               <div className="button-text">Get a consultation</div>
             </a>
           </div>
