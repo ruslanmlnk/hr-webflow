b8092a33070831eafe625a4f0ef2d22f3dadbdea
Ruslan Melnyk <kanevgoga2@gmail.com>
2026-01-29 13:43:43 +0200

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({
         {/* Preconnect для швидшого завантаження шрифтів */}
         <link href="https://fonts.googleapis.com" rel="preconnect" crossOrigin="anonymous" />
         <link href="https://fonts.gstatic.com" rel="preconnect" crossOrigin="anonymous" />
-        <link rel="icon" href="/favicon.ico" sizes="48x48" ></link>
         <link href="https://ajax.googleapis.com" rel="dns-prefetch" />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Inter:wght@400;500;600&display=swap"
