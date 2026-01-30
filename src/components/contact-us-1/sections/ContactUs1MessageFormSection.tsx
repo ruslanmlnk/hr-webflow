9e9bfc7234240c7d78029d32ba6dc9f22d0540cd
Ruslan Melnyk <kanevgoga2@gmail.com>
2026-01-30 14:56:16 +0200

    улук

--- src/components/contact-us-1/sections/ContactUs1MessageFormSection.tsx
+++ src/components/contact-us-1/sections/ContactUs1MessageFormSection.tsx
@@ -13,27 +13,16 @@ export default function ContactUs1MessageFormSection({ animation = "inview" }: P
     <div className="section without-top-spacing">
       <div className="base-container w-container">
         <div className="contacts-wrapper contacts-wrapper--top contacts-wrapper--form-wide contacts-wrapper--side-stretch">
-          {isInView ? (
-            <InView className="contacts-content-wrapper inview-fade-up inview-delay-0">
-              <ContactUs1SocialCards />
-            </InView>
-          ) : (
+         
             <div className="contacts-content-wrapper">
               <ContactUs1SocialCards />
             </div>
-          )}
           <div className="contacts-form-wrapper">
-            {isInView ? (
-              <InView className="inview-fade-up inview-delay-1">
-                <h3 className="contacts-form-title">Send us a message</h3>
-                <ContactUs1MessageForm />
-              </InView>
-            ) : (
+            
               <div>
                 <h3 className="contacts-form-title">Send us a message</h3>
                 <ContactUs1MessageForm />
               </div>
-            )}
           </div>
         </div>
       </div>
